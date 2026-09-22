b31efce6b496946e57cd680e2638edded89f4e7f
Zamorin Estate ERP <zamorinestatepvtltd.erp@gmail.com>
2026-09-22 12:23:12 +0530

    chore(reorg): align workspace directory topology, documentation, and platform assets

--- Platform/Android/settings.gradle.kts
+++ Platform/Android/settings.gradle.kts
@@ -29,5 +29,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "Zamorin Cafe ERP"
+rootProject.name = "ZamorinCafeERP-Android"
 include(":app")
--- windows/ZamorinCafeERP/App.xaml.cs
+++
@@ -1,8 +0,0 @@
-using System.Windows;
-
-namespace Zamorin.Cafe.ERP
-{
-    public partial class App : Application
-    {
-    }
-}
